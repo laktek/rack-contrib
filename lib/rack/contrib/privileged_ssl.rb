414ada9b222cb727bd7fc1f87b9459b42f603e30
lakshan <lakshan@web2media.net>
2009-03-29 07:11:41 +0530

    Rack::PrivilegedSsl - serve SSL only for a selected portion of subdomains (i.e. paid users only)

---
+++ lib/rack/contrib/privileged_ssl.rb
@@ -0,0 +1,42 @@
+module Rack
+  # Provides the option of serving SSL only for selected portion of subdomains (i.e. paid users).
+  # Other requests will be redirected to use standard HTTP.
+  # Initialize with an array of privileged subdomains.  
+  
+  class PrivilegedSsl 
+
+    def initialize(app, enabled_subdomains)
+      @app = app
+      @enabled_subdomains = enabled_subdomains
+    end
+
+    def call(env)
+      request = Rack::Request.new(env)
+      if request.scheme == "https" 
+       # check the given subdomain is in SSL enabled list
+       unless ssl_enabled?(subdomains(request.host).first)
+         modified_url = "http://#{request.host}#{request.fullpath}"
+         return [302, {'Location' => modified_url }, []]
+       end
+      end
+
+      @app.call(env)
+    end
+
+    def ssl_enabled?(account)
+      @enabled_subdomains.include? account
+    end
+
+    def subdomains(host, tld_length = 1)
+      return [] unless named_host?(host)
+
+      parts = host.split('.')
+      parts[0..-(tld_length+2)]
+    end
+
+    def named_host?(host)
+      !(host.nil? || /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(host))
+    end
+
+  end
+end
